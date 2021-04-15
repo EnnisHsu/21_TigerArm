cc9b19f4a7780958fb79c3e5f3c4392df0f9cef4
EnnisKoh <8762322@qq.com>
2021-04-16 01:40:25 +0800

    [Feat]T-Motor驱动测试

---
+++ USP/MotorCtrl.cpp
@@ -0,0 +1,80 @@
+#define _MotorCtrl_CPP_
+#include "MotorCtrl.h"
+
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+
+
+extern CAN_HandleTypeDef hcan1;
+
+AK80_V3 Motor_2(0x01,&hcan1),Motor_3(0x03,&hcan1);
+
+TaskHandle_t CAN1_TaskHandle,DogMotoCtrl_TaskHandle;
+
+void Service_MotoCtrl_Init()
+{
+	xTaskCreate((TaskFunction_t)Task_CAN1Receive,"CAN1_Receive",Tiny_Stack_Size,NULL,PriorityHigh,&CAN1_TaskHandle);
+	xTaskCreate((TaskFunction_t)Task_DogMotorCtrl,"DogMotoCtrl",Tiny_Stack_Size,NULL,PriorityHigh,&DogMotoCtrl_TaskHandle);
+}
+
+static void Convert_Data(CAN_RxMessage* input, COB_TypeDef* output)
+{
+  output->ID = input->header.StdId;
+  output->DLC = input->header.DLC;
+  memcpy(output->Data, input->data, output->DLC);
+}
+
+void CAN1_RxCpltCallback(CAN_RxBuffer *CAN_RxMessage)
+{
+	static COB_TypeDef CAN_RxCOB;
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+	Convert_Data(CAN_RxMessage,&CAN_RxCOB);
+	if ((CAN_RxMessage->data[1] &0x0F) !=0x00)
+	{
+		if (RMMotor_QueueHandle != NULL)
+		{
+			xQueueSendFromISR(RMMotor_QueueHandle,&CAN_RxCOB,&xHigherPriorityTaskWoken);
+		}
+	}
+}
+
+void Task_DogMotorCtrl(void)
+{
+	Motor_2.To_Into_Control();
+	Motor_3.To_Into_Control();
+	TickType_t xLastWakeTime_MotoCtrl;
+	for (;;)
+	{
+		xLastWakeTime_MotoCtrl = xTaskGetTickCount();
+		Motor_2.Out_Speed_Control(5,0.1);
+		Motor_3.Out_Speed_Control(5,0);
+		vTaskDelayUntil(&xLastWakeTime_MotoCtrl,5);
+	}
+}
+
+
+void Task_CAN1Receive(void)
+{
+	TickType_t xLastWakeTime_CAN1;
+	static COB_TypeDef Rx_COB;
+	for (;;)
+	{
+		xLastWakeTime_CAN1 = xTaskGetTickCount();
+		while (xQueueReceive(RMMotor_QueueHandle,&Rx_COB,0) == pdPASS)
+		{
+			switch (Rx_COB.ID)
+			{
+				case 0x01:
+					Motor_2.Update(Rx_COB.Data);
+					break;
+				case 0x03:
+					Motor_3.Update(Rx_COB.Data);
+					break;
+				default:
+					break;
+			}
+		}
+		vTaskDelayUntil(&xLastWakeTime_CAN1,1);
+	}
+}
---
+++ USP/MotorCtrl.h
@@ -0,0 +1,24 @@
+#ifndef _MotorCtrl_H_
+#define _MotorCtrl_H_
+
+#ifdef __cplusplus
+	#include "SRML.h"
+	#include "Dog_Motor.h"
+	extern "C"
+{
+#endif
+
+#include "System_DataPool.h"
+	
+void Service_MotoCtrl_Init();
+static void Convert_Data(CAN_RxMessage* input, COB_TypeDef* output);
+void CAN1_RxCpltCallback(CAN_RxBuffer *CAN_RxMessage);
+void Task_DogMotorCtrl(void);
+void Task_CAN1Receive(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+		
