5ef1d79450afe247b183d8d012101eda39b7f224
hheee <853856200@qq.com>
2021-05-02 11:42:49 +0800

    添加注释与部分actuator代码

--- USP/Actuator.cpp
+++ USP/Actuator.cpp
@@ -11,35 +11,89 @@
   * <tr><th>Date        <th>Version  <th>Author    				 <th>Description
   * <tr><td>2021-05-01  <td> 0.4.1   <td>EnnisKoh&Hehe     <td>Creator
   * </table>
-  *
-  ==============================================================================
-                            How to use this driver     
-  ==============================================================================
-    @note 
-		
-  ******************************************************************************
-  * @attention
-  * 
-  * if you had modified this file, please make sure your code does not have any 
-  * bugs, update the version Number, write dowm your name and the date. The most
-  * important thing is make sure the users will have clear and definite under-
-  * standing through your new brief.
-  *
-  * <h2><center>&copy; Copyright (c) 2019 - ~, SCUT-RobotLab Development Team.
-  * All rights reserved.</center></h2>
-  ******************************************************************************
   */
 
 
 
 /* Includes ------------------------------------------------------------------*/ 
 #include "Actuator.h"
+#include "stm32f4xx_hal.h"
 /* function prototypes -------------------------------------------------------*/
-//int Actuator_Classdef::suckerAct(){
+/**
+* @brief  actiong of actuator
+* @param  None
+* @retval 1 if success
+*/
+int Actuator_Classdef::suckerAct(){
 	//if negative pressure
-	//>threshold: 
-	//<=threshold:
-	
-//}
+	//>threshold:slirp
+	//<=threshold:stop
+	switch(Mode)
+	{
+		PreCatching:
+		{
+			turnMotor(SwitchOn);
+			airValve(SwitchOn);
+			break;
+		}
+		Catching:
+		{
+			turnMotor(SwitchOff);
+			airValve(SwitchOff);
+			break;
+		}
+		Release:
+		{
+			turnMotor(SwitchOff);
+			airValve(SwitchOn);
+			break;
+		}
+	default:return 0;break;
+	}
+	getValve();
+	switchMode();
+	return 1;
+}
+/**
+* @brief  
+* @param  
+* @retval 
+*/
+int Actuator_Classdef::turnMotor(SWITCH_MODE_TYPE mode){
+	//mode off gpio write pin 0
+	//mode on gpio write pin 1
+	return 1;
+}
+/**
+* @brief  
+* @param  
+* @retval 
+*/
+int Actuator_Classdef::airValve(SWITCH_MODE_TYPE mode){
+	//mode off gpio write pin 0
+	//mode on gpio write pin 1
+	return 1;
+}
 
-
+/**
+* @brief  
+* @param  
+* @retval 
+*/
+int Actuator_Classdef::getValve(){
+	//get negative pressure
+	//NegativePressure = 
+	return 1;
+}
+/**
+* @brief  
+* @param  
+* @retval 
+*/
+int Actuator_Classdef::switchMode(){
+	if(NegativePressure<NPThredshold)Mode = Catching;
+	else if(Cmd == Cmd_Catch)Mode = PreCatching;
+	else if(Cmd == Cmd_Release)Mode = Release;
+	else {return 0;}
+	return 1;
+}
